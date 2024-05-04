a7cfbd39f4d43acf6d3dc3dfad2c07e59dd9ea77
Brae <francesco.frediani@icloud.com>
2024-05-04 11:39:03 +0200

    Added italian date in dashboard.html and changed the standard Date.toString() method to generate the Date in italian

---
+++ script.js
@@ -0,0 +1,78 @@
+// Date.toString in Italian
+Date.toString = function() {
+    let ris = ''
+    let date = new Date()
+    let day = date.getDay()
+    let month = date.getMonth()
+    switch (day) {
+        case 0:
+            ris += 'Domenica'
+            break
+        case 1:
+            ris += 'Lunedì'
+            break
+        case 2:
+            ris += 'Martedì'
+            break
+        case 3:
+            ris += 'Mercoledì'
+            break
+        case 4:
+            ris += 'Giovedì'
+            break
+        case 5:
+            ris += 'Venerdì'
+            break
+        case 6:
+            ris += 'Sabato'
+            break
+
+    }
+
+    ris += ' ' + date.getDate() + ' '
+
+    switch (month) {
+        case 0:
+            ris += 'Gennaio'
+            break
+        case 1:
+            ris += 'Febbraio'
+            break
+        case 2:
+            ris += 'Marzo'
+            break
+        case 3:
+            ris += 'Aprile'
+            break
+        case 4:
+            ris += 'Maggio'
+            break
+        case 5:
+            ris += 'Giugno'
+            break
+        case 6:
+            ris += 'Luglio'
+            break
+        case 7:
+            ris += 'Agosto'
+            break
+        case 8:
+            ris += 'Settembre'
+            break
+        case 9:
+            ris += 'Ottobre'
+            break
+        case 10:
+            ris += 'Novembre'
+            break
+        case 11:
+            ris += 'Dicembre'
+            break
+    }
+
+    ris += ' ' + date.getFullYear()
+
+    return ris
+}
+
+document.getElementById("result").innerHTML = Date.toString()
